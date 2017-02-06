c09d3ac4a5606fb4b065983056f94fa533674ceb
phiggins <phiggins567@gmail.com>
2017-02-06 15:12:41 +0000

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+function initMap() {
+  var bravallenRd = {
+    lat: 55.0434806,
+    lng: -6.4926723
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: bravallenRd,
+  });
+  var marker = new google.maps.Marker({
+    position: bravallenRd,
+    map: map,
+    text: "test",
+    fontSize: 35,
+  });
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 6000);
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
