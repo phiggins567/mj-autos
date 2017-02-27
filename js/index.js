d3665fd0692425e294b58c9fa53dbe8686c7f84f
phiggins <phiggins567@gmail.com>
2017-02-27 08:18:07 +0000

    remove old js navigation

--- js/index.js
+++ js/index.js
@@ -31,12 +31,3 @@ function showSlides() {
   setTimeout(showSlides, 6000);
 }
 
-
-function navigation() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
